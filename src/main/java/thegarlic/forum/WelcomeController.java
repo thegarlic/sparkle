f8a596c7907c965aafef0759003f5299589ca958
SejongPark <devsejong@gmail.com>
2015-03-14 17:24:20 +0900

    jsp설정추가(정리작업 필요함.)

---
+++ src/main/java/thegarlic/forum/WelcomeController.java
@@ -0,0 +1,28 @@
+package thegarlic.forum;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+
+	@Value("test")
+    private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+	@RequestMapping("/foo")
+	public String foo(Map<String, Object> model) {
+		throw new RuntimeException("Foo");
+	}
+
+}
