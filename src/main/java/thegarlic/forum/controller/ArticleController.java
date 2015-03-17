b0e8073b8ce9d373688b42f20aaa9e7deb481182
yjc0703 <yjc0703@gmail.com>
2015-03-18 08:58:58 +0900

    rest 처리 롤백

--- src/main/java/thegarlic/forum/controller/ArticleController.java
+++ src/main/java/thegarlic/forum/controller/ArticleController.java
@@ -6,14 +6,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import thegarlic.forum.domain.Article;
+import thegarlic.forum.repository.ArticleRepository;
+
+import java.util.Date;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    ArticleRepository articleRepository;
 
-	@Autowired
-	ArticleApiController articleApiController;
 
     @RequestMapping(value = "/article", method = RequestMethod.GET)
     public String registerArticle() {
@@ -23,34 +25,36 @@ public class ArticleController {
 
     @RequestMapping(value = "/article", method = RequestMethod.POST)
     public String registerArticle(Article article) {
-    	article = articleApiController.registerArticle(article);
+        article.setWriteDate(new Date());
+        articleRepository.save(article);
         return "redirect:/article/" + article.getId();
     }
 
 
     @RequestMapping(value = "/article/{articleId}", method = RequestMethod.GET)
     public String readArticle(@PathVariable long articleId, Model model) {
-        Article article = articleApiController.readArticle(articleId);
+        Article article = articleRepository.findOne(articleId);
         model.addAttribute(article);
         return "viewArticle";
     }
 
     @RequestMapping(value = "/article/{articleId}", method = RequestMethod.DELETE)
     public String deleteArticle(@PathVariable long articleId) {
-        articleApiController.deleteArticle(articleId);
-        return "redirect:/article";
+        articleRepository.delete(articleId);
+        return "redirect:/Article";
     }
 
     @RequestMapping(value = "/article/{articleId}/modify", method = RequestMethod.GET)
     public String modifyArticle(@PathVariable long articleId, Model model) {
-        Article article = articleApiController.modifyArticle(articleId);
+        Article article = articleRepository.findOne(articleId);
         model.addAttribute(article);
         return "modifyArticle";
     }
 
     @RequestMapping(value = "/article/{articleId}/modify", method = RequestMethod.PUT)
     public String modifyArticle(Article article) {
-    	article = articleApiController.modifyArticle(article);
+        article.setWriteDate(new Date());
+        articleRepository.save(article);
         return "redirect:/article/" + article.getId();
     }
 
