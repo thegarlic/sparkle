f88a59b34c98207eadfbb6d6ec6584eb4a6a045b
SejongPark <devsejong@gmail.com>
2015-03-20 01:12:45 +0900

    어플리케이션 코드 indent를 탭 대신 space로 변경

--- src/main/java/thegarlic/forum/Application.java
+++ src/main/java/thegarlic/forum/Application.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	
+
     public static void main(String[] args) throws Exception {
-    	
-    	SpringApplication app = new SpringApplication(Application.class);
-    	app.setAdditionalProfiles("product");
-    	app.run(args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setAdditionalProfiles("product");
+        app.run(args);
     }
 }
