94d049f3f283fa4fb4cd989a7ee66c03981d79c4
yjc0703 <yjc0703@gmail.com>
2015-04-07 13:12:04 +0900

    add CORS filter

--- sparkle-rest-server/src/main/java/thegarlic/forum/config/AbstractConfig.java
+++ sparkle-rest-server/src/main/java/thegarlic/forum/config/AbstractConfig.java
@@ -1,5 +1,15 @@
 package thegarlic.forum.config;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.CommandLineRunner;
@@ -33,11 +43,31 @@ public abstract class AbstractConfig {
         
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
         
-        adapter.setDatabase(Database.DB2);
+        adapter.setDatabase(Database.H2);
         adapter.setGenerateDdl(true);
         adapter.setShowSql(true);
         
         return adapter;
+    }
+    
+    @Bean
+    private Filter SimpleCORSFilter() {
+        return new Filter() {
+            
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response,
+                    FilterChain chain) throws IOException, ServletException {
+                HttpServletResponse res = (HttpServletResponse)response;
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+                res.setHeader("Access-Control-Max-Age", "3600");
+                res.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+                chain.doFilter(request, res);
+            }
+            
+            @Override public void destroy() {}
+            @Override public void init(FilterConfig filterConfig) throws ServletException {}
+        };
     }
     
 }
