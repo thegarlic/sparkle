5a8ebb93a18b34981349fb0413afce7184f94c4b
yjc0703 <yjc0703@gmail.com>
2015-04-22 20:11:50 +0900

    mapper 를 분리구현해서 dto 구성 테스트

--- sparkle-rest-server/src/main/java/thegarlic/forum/dto/view/ArticleDto.java
+++ sparkle-rest-server/src/main/java/thegarlic/forum/dto/view/ArticleDto.java
@@ -1,8 +1,15 @@
 package thegarlic.forum.dto.view;
 
+import java.util.List;
+
 import lombok.Data;
+import thegarlic.forum.domain.Comment;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Data
+@JsonInclude(Include.NON_EMPTY)
 public class ArticleDto {
 	private long id;
 	private String writeDate;
@@ -10,4 +17,8 @@ public class ArticleDto {
 	private String author;
 	private String title;
 	private Integer readCount;
+	
+	private List<Comment> comments;
+	private Integer commentCount;
+	
 }
